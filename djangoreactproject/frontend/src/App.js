cd071c557d58a98163a9ac752c93f90914dcfcad
MAHOOR\Ebi <zare.math@gmail.com>
2019-07-27 11:29:49 -0400

    Done

--- djangoreactproject/frontend/src/App.js
+++ djangoreactproject/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import  CustomersList from './CustomersList'
 import  CustomerCreateUpdate  from './CustomerCreateUpdate'
@@ -9,18 +9,18 @@ import './App.css';
 const BaseLayout = () => (
   <div className="container-fluid">
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Django React Demo</a>
+  <a className="navbar-brand" href="#">Django React Demo CRM</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
       <a className="nav-item nav-link" href="/">CUSTOMERS</a>
-      <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
-
+      <a className="nav-item nav-link" href="/customer">CREATE NEW CUSTOMER</a>
+      
     </div>
   </div>
-</nav>
+</nav>  
 
     <div className="content">
       <Route path="/" exact component={CustomersList} />
